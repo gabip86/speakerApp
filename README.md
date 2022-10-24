1b50f51fd744443082b3281caeaf819744305ea2
eagalpb <gabriella.pal@ericsson.com>
2022-10-24 16:09:36 +0200

    Handling loading and error

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Pluralsight project
 
 **Designing React 17 Components** by Peter Kellner
+
+## Useful infos
+
+- run the application locally: `'npm run dev'`
--- src/components/SpeakersList.js
+++ src/components/SpeakersList.js
@@ -1,19 +1,31 @@
 import { useState, useEffect } from "react";
 import Speaker from "./Speaker";
 import { data } from "../../SpeakerData";
+import ReactPlaceHolder from "react-placeholder";
 
 const SpeakersList = ({ showSessions }) => {
   const [speakerData, setSpeakerData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasErrored, setHasErrored] = useState(false);
+  const [error, setError] = useState("");
 
   const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
   useEffect(() => {
     async function delayFunc() {
-      await delay(2000);
-      setSpeakerData(data);
+      try {
+        await delay(2000);
+        // throw "Had Error.";
+        setIsLoading(false);
+        setSpeakerData(data);
+      } catch (e) {
+        setIsLoading(false);
+        setHasErrored(true);
+        setError(e);
+      }
     }
     delayFunc();
-  });
+  }, []);
 
   const onFavoriteToggle = (id) => {
     const speakerRecPrevious = speakerData.find((rec) => {
@@ -29,22 +41,39 @@ const SpeakersList = ({ showSessions }) => {
     setSpeakerData(speakersDataNew);
   };
 
+  // Better to escape the if statement, than have multiple return statements
+  // KIS - Keep It Simple
+  if (hasErrored === true) {
+    return (
+      <div className="text-danger">
+        ERROR: <b>loading Speaker Data Failed {error}</b>
+      </div>
+    );
+  }
+
   return (
     <div className="container speakers-list">
-      <div className="row">
-        {speakerData.map(function (speaker) {
-          return (
-            <Speaker
-              key={speaker.id}
-              speaker={speaker}
-              showSessions={showSessions}
-              onFavoriteToggle={() => {
-                onFavoriteToggle(speaker.id);
-              }}
-            />
-          );
-        })}
-      </div>
+      <ReactPlaceHolder
+        type="media"
+        rows={15}
+        className="speakerslist-placeholder"
+        ready={isLoading === false}
+      >
+        <div className="row">
+          {speakerData.map(function (speaker) {
+            return (
+              <Speaker
+                key={speaker.id}
+                speaker={speaker}
+                showSessions={showSessions}
+                onFavoriteToggle={() => {
+                  onFavoriteToggle(speaker.id);
+                }}
+              />
+            );
+          })}
+        </div>
+      </ReactPlaceHolder>
     </div>
   );
 };
