76afdc988c231eed14b179d6a7c37bcbe8dae530
eagalpb <gabriella.pal@ericsson.com>
2022-10-24 16:24:13 +0200

    Refactor code to a hook

--- src/components/SpeakersList.js
+++ src/components/SpeakersList.js
@@ -1,45 +1,10 @@
-import { useState, useEffect } from "react";
 import Speaker from "./Speaker";
-import { data } from "../../SpeakerData";
 import ReactPlaceHolder from "react-placeholder";
+import useRequestSpeakers from "../hooks/useRequestSpeakers";
 
 const SpeakersList = ({ showSessions }) => {
-  const [speakerData, setSpeakerData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [hasErrored, setHasErrored] = useState(false);
-  const [error, setError] = useState("");
-
-  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
-  useEffect(() => {
-    async function delayFunc() {
-      try {
-        await delay(2000);
-        // throw "Had Error.";
-        setIsLoading(false);
-        setSpeakerData(data);
-      } catch (e) {
-        setIsLoading(false);
-        setHasErrored(true);
-        setError(e);
-      }
-    }
-    delayFunc();
-  }, []);
-
-  const onFavoriteToggle = (id) => {
-    const speakerRecPrevious = speakerData.find((rec) => {
-      return rec.id === id;
-    });
-    const speakerRecUpdated = {
-      ...speakerRecPrevious,
-      favorite: !speakerRecPrevious.favorite,
-    };
-    const speakersDataNew = speakerData.map((rec) => {
-      return rec.id === id ? speakerRecUpdated : rec;
-    });
-    setSpeakerData(speakersDataNew);
-  };
+  const { speakerData, isLoading, hasErrored, error, onFavoriteToggle } =
+    useRequestSpeakers(2000);
 
   // Better to escape the if statement, than have multiple return statements
   // KIS - Keep It Simple
